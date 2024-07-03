bc9f7322ab77f11a933891091c0619751c1c9919
Ayush Raj <ayushraj1268@gmail.com>
2024-07-03 17:05:59 +0530

    changes done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 import colors from "colors";
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(
